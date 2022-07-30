ccf153785c8b6811437507825042ed3b84e3cf5e
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-30 23:49:51 +0530

    Add files via upload

---
+++ 19_break_loop.py
@@ -0,0 +1,20 @@
+# n=-1
+# while(n<20):
+#     n+=2
+#     if n>12:
+#         break
+#     print(n)
+
+# n=0
+# while(n<10):
+#     n+=1
+#     if n==4:
+#         break
+#     print(n)
+
+# n=0
+# while(n<20):
+#     n+=2
+#     if(n==10):
+#         break
+#     print(n)
